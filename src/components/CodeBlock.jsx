6e39f15414ea30db4820d5000c9f2287262e8b97
tophercully <89924670+tophercully@users.noreply.github.com>
2024-01-18 14:11:08 -0600

    Code Highlighting fixed

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -2,9 +2,13 @@ import React, { useEffect } from "react";
 import hljs from "highlight.js";
 import javascript from "highlight.js/lib/languages/javascript";
 import glsl from "highlight.js/lib/languages/glsl";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+
+import reactHighlight from "react-highlight";
 
 import './CodeBlock.css'
 import "highlight.js/styles/monokai.css";
+import ReactHighlight from "react-highlight";
 
 
 
@@ -14,11 +18,13 @@ export const CodeBlock = (props) => {
         hljs.highlightAll()
     }, [props.code])
     return(
-        <pre>
-            <code className={language}>
-                {code}
-            </code>
-        </pre>
+        // <pre>
+        //     <code className={language}>
+        //         {code}
+        //     </code>
+        // </pre>
+
+        <ReactHighlight className={language}>{code}</ReactHighlight>
 
     )
 }
