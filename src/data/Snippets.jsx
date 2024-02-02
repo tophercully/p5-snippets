08f985e855e241435efd9ab47ce6b4e02e3d4396
tophercully <89924670+tophercully@users.noreply.github.com>
2024-02-02 16:23:09 -0600

    redesign

--- src/data/Snippets.jsx
+++ src/data/Snippets.jsx
@@ -489,8 +489,8 @@ export const snippets = {
             name:'Angle Between Coordinates',
             code: `
             function angBetween(x1, y1, x2, y2) {
-                return Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
-              }`,
+              return Math.atan2(y2 - y1, x2 - x1) * 180 / Math.PI;
+            }`,
               tags: 'angle, atan',
               author:'Chris McCully'
         },
